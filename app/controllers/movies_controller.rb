492990b289938fe686e9bc8d7bbc84cd66f86d48
Danny Sallurday <dannysallurday@gmail.com>
2019-10-14 20:13:04 -0700

    o

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all.order(params[:id])
+    @movies = Movie.all.order("#{params[:id]} ASC")
   end
 
   def new
